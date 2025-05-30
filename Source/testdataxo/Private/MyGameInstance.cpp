f00b777eed9cde6ab994e890adb07b3bc229a793
IlayBorisov <113771410+IlayBorisov@users.noreply.github.com>
2025-05-30 23:25:40 +0700

    fix: players moves

--- Source/testdataxo/Private/MyGameInstance.cpp
+++ Source/testdataxo/Private/MyGameInstance.cpp
@@ -1,2 +1,15 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 #include "MyGameInstance.h"
+#include "Misc/Guid.h"
+
+void UMyGameInstance::Init()
+{
+    Super::Init();
+
+    //Генерация CreatorID только если он пустой
+    if (CreatorID.IsEmpty())
+    {
+        CreatorID = FGuid::NewGuid().ToString(EGuidFormats::DigitsWithHyphens);
+        UE_LOG(LogTemp, Log, TEXT("Generated new ClientID from game instance: %s"), *CreatorID);
+    }
+}
